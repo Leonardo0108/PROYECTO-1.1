0eca24da6319537a0de4ac5cf7a2c7ababee3168
LEONARDO DE JESUS DE <91346313+Leonardo0108@users.noreply.github.com>
2021-10-15 14:32:19 +0000

    Reparamos los problemas que había y lo hicimos funcionar

--- promedio.js
+++ promedio.js
@@ -1,15 +1,29 @@
 function calcularPromedio() {
-var c1;
-var c2;
-var c3;
-var c4;
-var c5;
-var promedio;
-c1 = 10;
-c2 = 7.5;
-c3 = 9;
-c4 = 10;
-c5 = 10;
-promedio = c1 + c2 + c3 + c4 + c5 / 5;
-alert("Tu promedio es de: " + promedio);
+ var c1;
+ var c2;
+ var c3;
+ var c4;
+ var c5;
+ var promedio;
+ var campoDeTexto = document.getElementById("calificación1")
+ var contenido = campoDeTexto.value;
+ c1 = parseFloat(contenido);
+
+ campoDeTexto = document.getElementById("calificación2")
+ contenido = campoDeTexto.value;
+ c2 = parseFloat(contenido);
+ 
+ campoDeTexto = document.getElementById("calificación3")
+ contenido = campoDeTexto.value;
+ c3 = parseFloat(contenido);
+
+ campoDeTexto = document.getElementById("calificación4")
+ contenido = campoDeTexto.value;
+ c4 = parseFloat(contenido);
+
+ campoDeTexto = document.getElementById("calificación5")
+ contenido = campoDeTexto.value;
+ c5 = parseFloat(contenido);
+ promedio = (c1 + c2 + c3 + c4 + c5) / 5;
+ alert("Tu promedio es de: " + promedio);
 }
